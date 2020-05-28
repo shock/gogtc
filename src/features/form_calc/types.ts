410b57bda19aae68a666f07c386e6d87a0c9e2bb
shock <bill@greenling.com>
2020-05-28 16:51:25 -0500

    sort tierNum type top down. still may not be ordered

--- src/features/form_calc/types.ts
+++ src/features/form_calc/types.ts
@@ -21,18 +21,18 @@ export type Int = number & { __int__: void };
 export type Percentage = number;
 
 export enum TierNum {
-  T1 = 'T1',
-  T2 = 'T2',
-  T3 = 'T3',
-  T4 = 'T4',
-  T5 = 'T5',
-  T6 = 'T6',
-  T7 = 'T7',
-  T8 = 'T8',
-  T9 = 'T9',
-  T10 = 'T10',
-  T11 = 'T11',
   T12 = 'T12',
+  T11 = 'T11',
+  T10 = 'T10',
+  T9 = 'T9',
+  T8 = 'T8',
+  T7 = 'T7',
+  T6 = 'T6',
+  T5 = 'T5',
+  T4 = 'T4',
+  T3 = 'T3',
+  T2 = 'T2',
+  T1 = 'T1',
 }
 
 export enum TroopType {
