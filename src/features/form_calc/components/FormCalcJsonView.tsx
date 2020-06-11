1da3d2d2dd4047178182b810ab82b877118f12ad
shock <bill@greenling.com>
2020-06-11 17:55:29 -0500

    Fix typescript nag

--- src/features/form_calc/components/FormCalcJsonView.tsx
+++ src/features/form_calc/components/FormCalcJsonView.tsx
@@ -51,7 +51,7 @@ class FormCalcJsonViewBase extends React.Component<Props, State> {
 
   syntaxHighlight(json:string) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g, function (match) {
         var cls = 'number';
         if (/^"/.test(match)) {
             if (/:$/.test(match)) {
