86ba90922c62ac34e78a88a28eec0b5c8b3aedc1
shock <bill@greenling.com>
2020-06-14 16:31:04 -0500

    Add Json view toggle to formCalc selector

--- src/features/form_calc/components/FormCalcSelector.tsx
+++ src/features/form_calc/components/FormCalcSelector.tsx
@@ -25,7 +25,8 @@ type Props = ReturnType<typeof mapStateToProps> & typeof dispatchProps & FormCal
 type State = {
   formCalc: MFormCalc,
   formName: string,
-  debug: boolean
+  debug: boolean,
+  json: boolean
 }
 
 class FormCalcSelectorBase extends React.Component<Props, State> {
@@ -35,11 +36,13 @@ class FormCalcSelectorBase extends React.Component<Props, State> {
     this.state = {
       formCalc: TestLibrary.formCalcs[this.props.name],
       formName: this.props.name,
-      debug: false
+      debug: false,
+      json: false
     }
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleNameSubmit = this.handleNameSubmit.bind(this);
     this.handleDebugClick = this.handleDebugClick.bind(this);
+    this.handleJsonClick = this.handleJsonClick.bind(this);
   }
 
   componentDidMount() {
@@ -53,6 +56,10 @@ class FormCalcSelectorBase extends React.Component<Props, State> {
 
   handleDebugClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     this.setState({debug: !this.state.debug});
+  }
+
+  handleJsonClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    this.setState({json: !this.state.json});
   }
 
   handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -82,6 +89,50 @@ class FormCalcSelectorBase extends React.Component<Props, State> {
     return options;
   }
 
+  syntaxHighlight(json:string) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+  }
+
+  renderJsonView() {
+    const json = JSON.stringify(this.state.formCalc.asJsonObject(), null, 2);
+    return (
+      <Row>
+        <Col style={{textAlign: 'left'}}>
+          {/* <ReactJson
+            src={this.state.formCalcModel.asJsonObject()}
+            theme='monokai'
+          /> */}
+          <pre className="JsonView" dangerouslySetInnerHTML={{ __html: this.syntaxHighlight(json) }} />
+        </Col>
+      </Row>
+    )
+  }
+
+  renderCalcView() {
+    return (
+      <Row>
+        <Col>
+          <FormCalcView id={this.state.formCalc.name} debug={this.state.debug}/>
+        </Col>
+      </Row>
+    )
+  }
+
   render() {
     const debugButton = (
       <Button
@@ -89,7 +140,14 @@ class FormCalcSelectorBase extends React.Component<Props, State> {
         onClick={this.handleDebugClick}
       >Debug</Button>
     );
-    const msg = this.state.debug ? 'Hide Debug Info' : 'Show Debug Info';
+    const dMsg = this.state.debug ? 'Hide Debug Info' : 'Show Debug Info';
+    const jsonButton = (
+      <Button
+        variant={this.state.json ? "secondary" : "info"}
+        onClick={this.handleJsonClick}
+      >Json</Button>
+    );
+    const jMsg = this.state.json ? 'Hide Json' : 'Show Json';
 
     return (
       <React.Fragment>
@@ -114,15 +172,13 @@ class FormCalcSelectorBase extends React.Component<Props, State> {
               </Form.Group>
             </Form>
           </Col>
-          <Col sm={3}>
-            <TT tip={msg}>{debugButton}</TT>
+          <Col sm={4}>
+            <TT tip={jMsg}>{jsonButton}</TT>
+            &nbsp;&nbsp;&nbsp;
+            <TT tip={dMsg}>{debugButton}</TT>
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <FormCalcView id={this.state.formCalc.name} debug={this.state.debug}/>
-          </Col>
-        </Row>
+        { this.state.json ? this.renderJsonView() : this.renderCalcView() }
       </React.Fragment>
     );
   }
