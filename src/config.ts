afd7a2cdbfa306d810d99eb9b54a3b4c1c366962
shock <bill@greenling.com>
2020-06-15 22:47:40 -0500

    calc precision has to be view precision for best experience

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export default {
-  calcPrecision: 4, // precision used for big.js calculations
-  viewPrecision: 2 // decimal points shown in views
+  calcPrecision: 3, // precision used for big.js calculations
+  viewPrecision: 3 // decimal points shown in views
 }
