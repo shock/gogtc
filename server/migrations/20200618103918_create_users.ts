527b3ed7eea121c0f012d9114a1e95d1466fd5a4
shock <bill@greenling.com>
2020-06-18 12:54:24 -0500

    Connecting to postgres and create users with knex

---
+++ server/migrations/20200618103918_create_users.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema
+    .createTable('users', table => {
+      table.bigIncrements('id').primary()
+      table.string('name')
+      table.string('email')
+      table.string('pwdHash')
+      table.string('role')
+    })
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema
+    .dropTableIfExists('users')
+}
