bf1d66bacadfc762c72952ee245e6f6559c7b096
shock <bill@greenling.com>
2020-06-18 23:48:02 -0500

    add html views to server build.js

--- server/build-dev.js
+++ server/build-dev.js
@@ -9,7 +9,7 @@ try {
     fs.copySync('./src/public', './dist/public');
     fs.copySync('./src/views', './dist/views');
     // Transpile the typescript files
-    childProcess.exec('tsc --build tsconfig.prod.json');
+    childProcess.exec('tsc --build tsconfig.json');
 } catch (err) {
     console.log(err);
 }
