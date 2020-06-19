865c58eb0628adf6204113c5a0af54905a621a10
shock <bill@greenling.com>
2020-06-18 22:16:45 -0500

    Copy ./express_example to ./server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,23 @@
+import './loadEnv'; // Must be the first import
 import express from 'express';
 import path from 'path';
-const app = express();
+
+import app from './server';
+import logger from './shared/Logger';
 
 const rootPath = path.join(__dirname, '../..');
 app.use(express.static(path.join(rootPath, 'build')));
 
 app.get('/ping', function (req, res) {
- return res.send('pong');
+return res.send('pong');
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(rootPath, 'build', 'index.html'));
+    res.sendFile(path.join(rootPath, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 8080;
+// Start the server
+const port = Number(process.env.PORT || 8080);
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    logger.info('Express server started on port: ' + port);
 });
