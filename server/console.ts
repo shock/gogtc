1cce1dc0f318872d8a6e13f23d91255085dcb711
shock <bill@greenling.com>
2020-06-18 21:27:47 -0500

    Create REPL console

---
+++ server/console.ts
@@ -0,0 +1,56 @@
+/**
+ * add console.js at ther root of your app
+ * Modify to make it more helpful for your project
+ * > Note: `esm` adds ES6 support in Node
+ * In package.json
+ *  ...
+ *  "scripts": {
+ *    "start": "nodemon -r esm ./server.js"
+ *    "console": "node -r esm --experimental-repl-await console",
+ *  }
+ *
+ * check whether you have yarn installed
+ * $ yarn -v
+ * 1.12.3
+ * If not then install it using
+ * $ npm install -g yarn
+ * /usr/local/bin/yarn -> /usr/local/lib/node_modules/yarn/bin/yarn.js
+ *  /usr/local/bin/yarnpkg -> /usr/local/lib/node_modules/yarn/bin/yarn.js
+ *  + yarn@1.12.3
+ * updated 1 package in 1.459s
+ *
+ * Now you can run command
+ * $ yarn console
+ * app > // Here we have all sequelize models
+ * app > (await User.findOne()).toJSON() // return {..user} json object
+ */
+
+import repl from 'repl';
+import * as models from './src/models';
+import Knex from 'knex'
+import { Model, ForeignKeyViolationError, ValidationError } from 'objection'
+import knexConfig from './knexfile'
+const replHistory = require('repl.history')
+
+// replHistory(repl, process.env.HOME + '/.node_history')
+
+Object.keys(models).forEach(modelName => {
+  global[modelName] = models[modelName];
+});
+
+// Initialize knex.
+const knex = Knex(knexConfig.development)
+
+// Bind all Models to a knex instance. If you only have one database in
+// your server this is all you have to do. For multi database systems, see
+// the Model.bindKnex() method.
+Model.knex(knex)
+
+
+const replServer = repl.start({
+  prompt: 'server > '
+});
+
+require('repl.history')(replServer, process.env.HOME + '/.node_history');
+
+replServer.context.db = models;
