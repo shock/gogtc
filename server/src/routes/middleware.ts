b20e6f4efa9c67bb7702749ecae38c66c91bfe69
shock <bill@greenling.com>
2020-06-25 16:31:19 -0500

    reformat NFC

--- server/src/routes/middleware.ts
+++ server/src/routes/middleware.ts
@@ -12,45 +12,45 @@ const jwtService = new JwtService();
 
 // Middleware to verify if user is an admin
 export const adminMW = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        // Get json-web-token
-        const jwt = req.signedCookies[cookieProps.key];
-        if (!jwt) {
-            throw Error('JWT not present in signed cookie.');
-        }
-        // Make sure user role is an admin
-        const clientData = await jwtService.decodeJwt(jwt);
-        if (clientData.role === UserRoles.Admin) {
-            res.locals.userId = clientData.id;
-            next();
-        } else {
-            throw Error('JWT not present in signed cookie.');
-        }
-    } catch (err) {
-        return res.status(UNAUTHORIZED).json({
-            error: err.message,
-        });
-    }
+	try {
+		// Get json-web-token
+		const jwt = req.signedCookies[cookieProps.key];
+		if (!jwt) {
+			throw Error('JWT not present in signed cookie.');
+		}
+		// Make sure user role is an admin
+		const clientData = await jwtService.decodeJwt(jwt);
+		if (clientData.role === UserRoles.Admin) {
+			res.locals.userId = clientData.id;
+			next();
+		} else {
+			throw Error('JWT not present in signed cookie.');
+		}
+	} catch (err) {
+		return res.status(UNAUTHORIZED).json({
+			error: err.message,
+		});
+	}
 };
 
 // Middleware to verify if user is an admin
 export const userMW = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        // Get json-web-token
-        const jwt = req.signedCookies[cookieProps.key];
-        if (!jwt) {
-            throw Error('JWT not present in signed cookie.');
-        }
-        // we have a web token, set the userId in req.locals
-        const clientData = await jwtService.decodeJwt(jwt);
-        res.locals.userId = clientData.id;
-        if (clientData.role === UserRoles.Admin) {
-            res.locals.admin = true;
-        }
-        next();
-    } catch (err) {
-        return res.status(UNAUTHORIZED).json({
-            error: err.message,
-        });
-    }
+	try {
+		// Get json-web-token
+		const jwt = req.signedCookies[cookieProps.key];
+		if (!jwt) {
+			throw Error('JWT not present in signed cookie.');
+		}
+		// we have a web token, set the userId in req.locals
+		const clientData = await jwtService.decodeJwt(jwt);
+		res.locals.userId = clientData.id;
+		if (clientData.role === UserRoles.Admin) {
+			res.locals.admin = true;
+		}
+		next();
+	} catch (err) {
+		return res.status(UNAUTHORIZED).json({
+			error: err.message,
+		});
+	}
 };
